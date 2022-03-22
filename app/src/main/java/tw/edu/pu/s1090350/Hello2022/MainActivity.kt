c689c3aea4932b240d7c9147a509d4e7aa15c1b0
s1090350 <s1090350@gm.pu.edu.tw>
2022-03-22 11:33:33 +0800

    合併後，再次修改txv顯示的文字

--- app/src/main/java/tw/edu/pu/s1090350/Hello2022/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1090350/Hello2022/MainActivity.kt
@@ -8,18 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var txv: TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Hello tcyang"
-        TestMaster()
-    }
 
-    fun TestMaster(){
         var txv:TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Master分支"
-        TestDEV()
-    }
-    fun TestDEV(){
-        var txv:TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Dev分支"
+        //txv.text = "Hello tcyang"
+        //TestDEV()
+        //TestMaster()
+        txv.text = "嗨，我是楊子青"
+
     }
 }

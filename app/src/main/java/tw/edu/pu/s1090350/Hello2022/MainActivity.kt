246e0741f704b06ca1a2b1b5a296b0d85d494177
s1090350 <s1090350@gm.pu.edu.tw>
2022-03-22 09:51:23 +0800

    第二次 Commit(修改文字元件)

--- app/src/main/java/tw/edu/pu/s1090350/Hello2022/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1090350/Hello2022/MainActivity.kt
@@ -2,10 +2,14 @@ package tw.edu.pu.s1090350.Hello2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var txv: TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Hello tcyang"
+
     }
 }

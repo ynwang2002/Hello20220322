8ba5f4db7635fa5e04d6de499dd49ad72b862686
s1090350 <s1090350@gm.pu.edu.tw>
2022-03-22 09:37:21 +0800

    第一次 Commit

---
+++ app/src/main/java/tw/edu/pu/s1090350/Hello2022/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.s1090350.Hello2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
